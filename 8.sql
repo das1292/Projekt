80808767a4a2f918a7f75f5d1980d1f041aeff57
das1292 <73549342+das1292@users.noreply.github.com>
2021-01-07 16:54:09 +0100

    Delete 8.sql

--- 8.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW `zadanie8` AS
-select producent.nazwa, producent.rodzaj_towaru
-from producent
-where producent.producentID in (select mieso.producent
-                 		from mieso
-				where mieso.cena >=10 and mieso.cena<=50 );
