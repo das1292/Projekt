2add9ecd2c8e752069dae5de426bbe02836550ce
das1292 <73549342+das1292@users.noreply.github.com>
2021-01-07 16:53:45 +0100

    Delete 10.sql

--- 10.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW `zadanie10` AS
-select stanowisko.rodzaj, stanowisko.sprzęt
-from stanowisko
-where stanowisko.stanowiskoID in (select pracownik.stanowisko
-from pracownik
-where pracownik.nazwisko LIKE '%i');
